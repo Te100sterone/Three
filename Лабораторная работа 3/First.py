77427ebcaa1ecda7534b76e9ce2a4e7972c38378
Te100sterone <148277474+Te100sterone@users.noreply.github.com>
2023-11-02 00:10:09 +0300

    Add files via upload

---
+++ Лабораторная работа 3/First.py
@@ -0,0 +1,15 @@
+# TODO Напишите функцию для поиска индекса товара
+def finding(items_, find_):
+    for number, needed_item in enumerate(items_):
+        if needed_item == find_:
+            return number
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = finding(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
